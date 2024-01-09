c782a095da5d94c5a5ba2375769854ab24faf80b
Toma Milkov <tmilkov@appolica.com>
2024-01-09 12:40:58 +0200

    Added create account page.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,45 @@ import Navigation from './components/navigation/Navigation';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Terms from './pages/terms/Terms';
 import Privacy from './pages/privacy/Privacy';
+import CreateAccount from './pages/createAccount/CreateAccount';
 
 
 function App() {
   return (
     <Router>
       <main className="content">
-        <Navigation />
-
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/" element={<CreateAccount />} />
+          <Route path="/home" element={
+            <>
+              <Navigation />
+              <Home />
+            </>
+          } />
+          <Route path="/transactions" element={
+            <>
+              <Navigation />
+              <Transactions />
+            </>
+          } />
+          <Route path="/reports" element={
+            <>
+              <Navigation />
+              <Reports />
+            </>
+          } />
+          <Route path="/terms" element={
+            <>
+              <Navigation />
+              <Terms />
+            </>
+          } />
+          <Route path="/privacy" element={
+            <>
+              <Navigation />
+              <Privacy />
+            </>
+          } />
         </Routes>
       </main>
     </Router>
